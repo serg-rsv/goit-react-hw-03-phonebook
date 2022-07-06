940f74bf30f2da26cbc57b28d553ee4fe1393af8
Serhii Rybak <rybaksw@gmail.com>
2022-07-07 00:03:00 +0300

    fix(ContactForm): fix regex patterns

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,10 +5,10 @@ import { FormContainer } from './FormContainer.styled';
 import { Button } from 'components/Button.styled';
 
 const nameRegexPattern = new RegExp(
-  "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/
 );
 const numberRegexPattern = new RegExp(
-  '\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}'
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
 );
 
 export const ContactForm = ({ addContact }) => {
